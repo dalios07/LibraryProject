dd09676fdfa4a63e13140e4ad4a1ddd4c0549d99
mathlouthi <medali.mathlouthi@aderivatives.com>
2026-06-22 15:50:48 +0100

    corriger compilation error

--- ProjectBackend/demo/src/main/java/com/example/demo/LibraryBackendApplication.java
+++ ProjectBackend/demo/src/main/java/com/example/demo/LibraryBackendApplication.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootApplication
 @EnableJpaAuditing
